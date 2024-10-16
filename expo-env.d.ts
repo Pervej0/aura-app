fecf008c497f63cb2433e0a7f2afdcedeb7faf86
pervej0 <mdparvez222khan@gmail.co>
2024-10-16 17:05:35 +0600

    constants file added

--- expo-env.d.ts
+++ expo-env.d.ts
@@ -1,3 +1,7 @@
 /// <reference types="expo/types" />
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpeg";
+declare module "*.jpg";
 
 // NOTE: This file should not be edited and should be in your git ignore
